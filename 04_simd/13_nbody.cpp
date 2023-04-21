d25e676c28022b741069889bbbc16a80b9066a30
Kei GORAKU <graku120808@gmail.com>
2023-04-21 17:19:01 +0900

    4/24 kadai

--- 04_simd/13_nbody.cpp
+++ 04_simd/13_nbody.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <cmath>
 
+#include <immintrin.h>
+
 int main() {
   const int N = 8;
   float x[N], y[N], m[N], fx[N], fy[N];
@@ -11,16 +13,40 @@ int main() {
     m[i] = drand48();
     fx[i] = fy[i] = 0;
   }
+  __m256 x_vec = _mm256_load_ps(x);
+  __m256 y_vec = _mm256_load_ps(y);
+  __m256 fx_vec = _mm256_load_ps(fx);
+  __m256 fy_vec = _mm256_load_ps(fy);
   for(int i=0; i<N; i++) {
-    for(int j=0; j<N; j++) {
-      if(i != j) {
-        float rx = x[i] - x[j];
-        float ry = y[i] - y[j];
-        float r = std::sqrt(rx * rx + ry * ry);
-        fx[i] -= rx * m[j] / (r * r * r);
-        fy[i] -= ry * m[j] / (r * r * r);
-      }
-    }
+    __m256 rx_vec = _mm256_sub_ps(x_vec, _mm256_set1_ps(x[i])); // rx[] = x[] - x[i], Distance between each point (x[]) and the i-th point (x[i])
+    __m256 ry_vec = _mm256_sub_ps(y_vec, _mm256_set1_ps(y[i])); // ry[] = y[] - y[i]
+    __m256 one_rth_vec = _mm256_rsqrt_ps( // 1/r = 1/sqrt(rx^2 + ry^2)
+      _mm256_add_ps(
+        _mm256_mul_ps(rx_vec, rx_vec), _mm256_mul_ps(ry_vec, ry_vec)
+      )
+    );
+    __m256 one_rth_vec3 = _mm256_mul_ps(_mm256_mul_ps(one_rth_vec, one_rth_vec), one_rth_vec); // (1/r)^3
+
+    fx_vec = _mm256_sub_ps( // fx[] -= rx[] * m[i] * (1/r)^3
+      fx_vec,
+      _mm256_blend_ps(
+        _mm256_mul_ps(_mm256_mul_ps(rx_vec, _mm256_set1_ps(m[i])), one_rth_vec3),
+        _mm256_setzero_ps(),
+        0b00000001<<i // mask i-th (0b..1..)
+      )
+    );
+    fy_vec = _mm256_sub_ps( // fy[] -= ry[] * m[i] * (1/r)^3
+      fy_vec,
+      _mm256_blend_ps(
+        _mm256_mul_ps(_mm256_mul_ps(ry_vec, _mm256_set1_ps(m[i])), one_rth_vec3),
+        _mm256_setzero_ps(),
+        0b00000001<<i // mask i-th (0b..1..)
+      )
+    );
+  }
+  _mm256_store_ps(fx, fx_vec);
+  _mm256_store_ps(fy, fy_vec);
+  for (int i=0; i<N; i++) {
     printf("%d %g %g\n",i,fx[i],fy[i]);
   }
 }
