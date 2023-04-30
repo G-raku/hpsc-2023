6b1b261771843b49ba070e59f9be25614105dffd
Kei GORAKU <graku120808@gmail.com>
2023-05-01 08:16:54 +0900

    kadai

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -26,6 +26,7 @@ int main(int argc, char** argv) {
   MPI_Datatype MPI_BODY;
   MPI_Type_contiguous(5, MPI_DOUBLE, &MPI_BODY);
   MPI_Type_commit(&MPI_BODY);
+  /*
   for(int irank=0; irank<size; irank++) {
     MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
     MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -40,7 +41,26 @@ int main(int argc, char** argv) {
         }
       }
     }
+  }//*/
+  MPI_Win win;
+  MPI_Win_create(jbody, N/size*5*sizeof(double), 5*sizeof(double), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+  for(int irank=0; irank<size; irank++) {
+    MPI_Win_fence(0, win);
+    MPI_Put(jbody, N/size, MPI_BODY, send_to, 0, N/size, MPI_BODY, win);
+    MPI_Win_fence(0, win);
+    for(int i=0; i<N/size; i++) {
+      for(int j=0; j<N/size; j++) {
+        double rx = ibody[i].x - jbody[j].x;
+        double ry = ibody[i].y - jbody[j].y;
+        double r = std::sqrt(rx * rx + ry * ry);
+        if (r > 1e-15) {
+          ibody[i].fx -= rx * jbody[j].m / (r * r * r);
+          ibody[i].fy -= ry * jbody[j].m / (r * r * r);
+        }
+      }
+    }
   }
+  MPI_Win_free(&win);
   for(int irank=0; irank<size; irank++) {
     MPI_Barrier(MPI_COMM_WORLD);
     if(irank==rank) {
