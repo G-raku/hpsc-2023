06f354ff92a775bd1c4e5b263cb0320beee96909
Kei GORAKU <graku120808@gmail.com>
2023-05-27 06:08:37 +0900

    paralelized align

--- 06_cuda/13_bucket_sort.cu
+++ 06_cuda/13_bucket_sort.cu
@@ -1,16 +1,22 @@
 #include <cstdio>
 #include <cstdlib>
 
+__device__ void align(int* key, int offset, int quantity, int n) {
+  for (int j=0; j<quantity; j++) {
+    key[offset+j] = n;
+  }
+}
+
 __global__ void bucket_sort(int* key, int range) {
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   extern __shared__ int bucket[];
   atomicAdd(&bucket[key[i]], 1);
   __syncthreads();
   if (threadIdx.x == 0) {
-    for (int j=0, k=0; j<range; j++) {
-      for (; bucket[j]>0; bucket[j]--) {
-        key[k++] = j;
-      }
+    int offset = 0;
+    for (int j=0; j<range; j++) {
+      align(key, offset, bucket[j], j);
+      offset += bucket[j];
     }
   }
 }
